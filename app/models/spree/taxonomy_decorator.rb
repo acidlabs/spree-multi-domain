476398b3206083d700646ba3d5bb1eb0de5fd40e
mespina <mespina@acid.cl>
2014-02-20 15:20:54 -0300

    fix scope

--- app/models/spree/taxonomy_decorator.rb
+++ app/models/spree/taxonomy_decorator.rb
@@ -1,5 +1,5 @@
 Spree::Taxonomy.class_eval do
   belongs_to :store
 
-  scope :by_store, -> {|store| where("spree_taxonomies.store_id = ?", store)}
+  scope :by_store, lambda {|store| where("spree_taxonomies.store_id = ?", store)}
 end
