06ec1c777e2b9a1b661c44fdd5c2ff5c5c9b4c15
mespina <mespina@acid.cl>
2014-03-06 20:24:14 -0300

    no elimina tiendas al actualizar propiedades

--- app/controllers/spree/admin/products_controller_decorator.rb
+++ app/controllers/spree/admin/products_controller_decorator.rb
@@ -3,7 +3,10 @@ Spree::Admin::ProductsController.class_eval do
 
   private
   def set_stores
-    @product.store_ids = nil unless params[:product].key? :store_ids
+    # Si estoy actualizando las propiedades del producto no hago nada
+    return if params[:product].key?(:product_properties_attributes)
+
+    @product.store_ids = nil unless params[:product].key?(:store_ids)
   end
 
 end
