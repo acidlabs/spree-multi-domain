fd9ab4ce4c447296d82a67b5dae9115ced5d0a8e
mespina <mespina@acid.cl>
2014-03-11 16:51:08 -0400

    Sort taxonomies by position into #get_taxonomies

--- app/helpers/spree/base_helper_decorator.rb
+++ app/helpers/spree/base_helper_decorator.rb
@@ -2,7 +2,7 @@ module Spree
   BaseHelper.class_eval do
     def get_taxonomies
       @taxonomies ||= current_store.present? ? Spree::Taxonomy.by_store(current_store) : Spree::Taxonomy
-      @taxonomies = @taxonomies.includes(:root => :children)
+      @taxonomies = @taxonomies.order(:position).includes(:root => :children)
       @taxonomies
     end
   end
--- app/helpers/spree/products_helper_decorator.rb
+++ app/helpers/spree/products_helper_decorator.rb
@@ -3,7 +3,7 @@ module Spree
     ProductsHelper.class_eval do
       def get_taxonomies
         @taxonomies ||= current_store.present? ? Spree::Taxonomy.by_store(current_store) : Spree::Taxonomy
-        @taxonomies = @taxonomies.includes(:root => :children)
+        @taxonomies = @taxonomies.order(:position).includes(:root => :children)
         @taxonomies
       end
     end
