327a38f4842d23ddff2d18e2484e5dbc93ae672b
Gonzalo <gmoreno@acid.cl>
2014-03-13 14:37:25 -0400

    Add products per page by store

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,6 +2,7 @@ FactoryGirl.define do
   factory :store, :class => Spree::Store do
     name 'My store'
     code 'my_store'
+    products_per_page 20
     domains 'www.example.com' # makes life simple, this is the default
     # integration session domain
   end
