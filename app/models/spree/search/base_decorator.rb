564505fcde5dbfc6eb21b6b9d7688eaf1a476ef4
mespina <mespina@acid.cl>
2014-03-18 20:22:32 -0400

    fix app/models/spree/search/base_decorator.rb reviso current_store antes de preguntar por :products_per_page de la store

--- app/models/spree/search/base_decorator.rb
+++ app/models/spree/search/base_decorator.rb
@@ -5,8 +5,17 @@ protected
     @properties[:taxon] = params[:taxon].blank? ? nil : Spree::Taxon.find(params[:taxon])
     @properties[:keywords] = params[:keywords]
     @properties[:search] = params[:search]
-    current_store = params[:current_store_id].blank? ? nil : Spree::Store.find(params[:current_store_id])
-    default_per_page = current_store.products_per_page.blank? ? Spree::Config[:products_per_page] : current_store.products_per_page
+
+    if params[:current_store_id]
+      current_store = Spree::Store.find(params[:current_store_id])
+
+      if current_store
+        default_per_page = current_store.products_per_page.blank? ? Spree::Config[:products_per_page] : current_store.products_per_page
+      else
+        default_per_page = Spree::Config[:products_per_page]
+      end
+    end
+
     per_page = params[:per_page].to_i
     @properties[:per_page] = per_page > 0 ? per_page : default_per_page
     @properties[:page] = (params[:page].to_i <= 0) ? 1 : params[:page].to_i
